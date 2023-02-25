1543b0fd2cf2ec04840c7173f7050ca4a0e02e23
Ignacio Planas <ignacioplanas2001@gmail.com>
2023-02-25 19:13:36 +0100

    Create Tutorial.java

---
+++ src/main/java/acme/entities/tutorials/Tutorial.java
@@ -0,0 +1,45 @@
+
+package acme.entities.tutorials;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Tutorial extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Column(unique = true)
+	@NotBlank
+	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
+	protected String			code;
+
+	@NotBlank
+	@Length(min = 1, max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(min = 1, max = 100)
+	protected String			abstactStr;
+
+	@NotBlank
+	@Length(min = 1, max = 100)
+	protected String			goals;
+
+	protected Double			estimatedTotalTime;
+
+}
