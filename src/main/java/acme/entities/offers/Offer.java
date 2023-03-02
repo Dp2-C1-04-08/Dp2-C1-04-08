0b295555a833c792a95d21241d2a901fd5d0f14a
Ignacio Planas <ignacioplanas2001@gmail.com>
2023-03-02 17:12:00 +0100

    Update Offer.java

--- src/main/java/acme/entities/offers/Offer.java
+++ src/main/java/acme/entities/offers/Offer.java
@@ -6,21 +6,23 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
-import javax.validation.constraints.PositiveOrZero;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
+import acme.framework.components.datatypes.Money;
+import acme.framework.data.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Offer {
+public class Offer extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
 
@@ -52,8 +54,8 @@ public class Offer {
 	protected Date				endDate;
 
 	@NotNull
-	@PositiveOrZero
-	protected double			price;
+	@Valid
+	protected Money				price;
 
 	@URL
 	protected String			link;
