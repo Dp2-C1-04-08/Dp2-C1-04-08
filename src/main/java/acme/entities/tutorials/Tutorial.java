27a812f98affcf17c4f1d6e5cbc508118a5a80b4
Ignacio Planas <ignacioplanas2001@gmail.com>
2023-03-03 12:52:11 +0100

    Update Tutorial.java
    #65

--- src/main/java/acme/entities/tutorials/Tutorial.java
+++ src/main/java/acme/entities/tutorials/Tutorial.java
@@ -3,13 +3,17 @@ package acme.entities.tutorials;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.PositiveOrZero;
 
 import org.hibernate.validator.constraints.Length;
 
 import acme.framework.data.AbstractEntity;
+import acme.roles.Assistant;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -42,14 +46,15 @@ public class Tutorial extends AbstractEntity {
 	protected String			goals;
 
 	@NotNull
+	@PositiveOrZero
 	protected Double			estimatedTotalTime;
 
 	protected boolean			draft;
 
-	//	@NotNull
-	//	@Valid
-	//  @ManyToOne(optional = false)
-	//	protected Assistant			assistant	;
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Assistant			assistant;
 
 	//	@NotNull
 	//	@Valid
