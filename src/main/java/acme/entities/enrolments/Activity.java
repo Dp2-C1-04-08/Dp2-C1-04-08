58a90687fc5f87a2bfe29c08098807665d36442f
Ernesto <ertorivero2661@gmail.com>
2023-03-05 11:14:39 +0100

    Cambiar de AbstractRole a AbstractEntity

--- src/main/java/acme/entities/enrolments/Activity.java
+++ src/main/java/acme/entities/enrolments/Activity.java
@@ -12,14 +12,14 @@ import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
-import acme.framework.data.AbstractRole;
+import acme.framework.data.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Activity extends AbstractRole {
+public class Activity extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
 
