c48269d741d44d4c2981503eca7e0ebe5f4bcd16
rubcasfer2 <100583036+rubcasfer2@users.noreply.github.com>
2023-03-06 20:39:04 +0100

    Merge Practicum y PracticumSessions
    Fusion de las ramas para poder popular la base datos con los datos de prueba

---
+++ src/main/java/acme/entities/practicums/Practicum.java
@@ -0,0 +1,39 @@
+
+package acme.entities.practicums;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Practicum extends AbstractEntity {
+
+	protected static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
+	protected String			code;
+
+	@NotBlank
+	@Length(min = 0, max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(min = 0, max = 100)
+	protected String			goals;
+
+	protected Integer			timePlus10;
+
+	protected Integer			timeMinus10;
+
+}
