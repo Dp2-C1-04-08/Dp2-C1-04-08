73fba7a1e89e85a40d5afdb64c231e6f85ab8637
manbarjim2 <80253313+manbarjim2@users.noreply.github.com>
2023-03-07 18:40:19 +0100

    Actualizar Course.java

--- src/main/java/acme/entities/courses/Course.java
+++ src/main/java/acme/entities/courses/Course.java
@@ -3,6 +3,7 @@ package acme.entities.courses;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
@@ -11,6 +12,7 @@ import org.hibernate.validator.constraints.URL;
 
 import acme.framework.components.datatypes.Money;
 import acme.framework.data.AbstractEntity;
+import acme.roles.Lecturer;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -43,5 +45,8 @@ public class Course extends AbstractEntity {
 
 	@URL
 	protected String			link;
+
+	@ManyToOne
+	protected Lecturer			lecturer;
 
 }
