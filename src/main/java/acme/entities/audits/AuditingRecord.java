a5ffc86247c8fa6cd7a609ecdfd1af722976956c
siadan <siadan@DESKTOP-ME0QPDA>
2023-03-09 13:57:30 +0100

    Created AuditingRecord entity

---
+++ src/main/java/acme/entities/audits/AuditingRecord.java
@@ -0,0 +1,61 @@
+
+package acme.entities.audits;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class AuditingRecord extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Length(min = 1, max = 75)
+	protected String			subject;
+
+	@NotBlank
+	@Length(min = 1, max = 100)
+	protected String			assesssment;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	@Past
+	protected Date				startDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	@Past
+	protected Date				endDate;
+
+	protected MarkValue			mark;
+
+	@URL
+	protected String			link;
+
+	@Valid
+	@NotNull
+	@ManyToOne(optional = false)
+	protected Audit				audit;
+
+}
