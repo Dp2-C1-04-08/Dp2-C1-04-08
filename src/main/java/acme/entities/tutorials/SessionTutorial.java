d0501b2d58047858df45cfdc7cdbee3cfa0706e4
Ignacio Planas <ignacioplanas2001@gmail.com>
2023-03-09 16:59:20 +0100

    Update SessionTutorial.java

--- src/main/java/acme/entities/tutorials/SessionTutorial.java
+++ src/main/java/acme/entities/tutorials/SessionTutorial.java
@@ -4,14 +4,17 @@ package acme.entities.tutorials;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
+import acme.entities.courses.Nature;
 import acme.framework.data.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -35,7 +38,8 @@ public class SessionTutorial extends AbstractEntity {
 	@Length(min = 1, max = 100)
 	protected String			abstractStr;
 
-	protected boolean			isTheory;
+	@NotNull
+	protected Nature			type;
 
 	@URL
 	protected String			link;
@@ -50,9 +54,9 @@ public class SessionTutorial extends AbstractEntity {
 
 	protected boolean			draft;
 
-	//	@NotNull
-	//	@Valid
-	//	@ManyToOne(optional = false)
-	//	protected Tutorial			tutorial;
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Tutorial			tutorial;
 
 }
