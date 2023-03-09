cbc0bd730d572811ee4e48e9ac67c4181cb8c253
manbarjim2 <80253313+manbarjim2@users.noreply.github.com>
2023-03-09 16:36:00 +0100

    Mejorar Lecture

--- src/main/java/acme/entities/courses/Course.java
+++ src/main/java/acme/entities/courses/Course.java
@@ -1,23 +1,37 @@
 
 package acme.entities.courses;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
+import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
+import acme.framework.components.datatypes.Money;
 import acme.framework.data.AbstractEntity;
+import acme.roles.Lecturer;
+import lombok.Getter;
+import lombok.Setter;
 
-public class Lecture extends AbstractEntity {
+@Entity
+@Getter
+@Setter
+public class Course extends AbstractEntity {
 	// Serialisation identifier -----------------------------------------------
 
 	protected static final long	serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
+
+	@Column(unique = true)
+	@NotBlank
+	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
+	protected String			code;
 
 	@NotBlank
 	@Length(min = 1, max = 75)
@@ -25,17 +39,13 @@ public class Lecture extends AbstractEntity {
 
 	@NotBlank
 	@Length(min = 1, max = 100)
-	protected String			abstractStr;
+	protected String			abstactStr;
+
+	protected Nature			courseType;
 
 	@NotNull
-	@Positive
-	protected Double			estimatedLearningTime;
-
-	@NotBlank
-	@Length(min = 1, max = 100)
-	protected String			body;
-
-	protected Nature			lectureType;
+	@Valid
+	protected Money				retailPrice;
 
 	@URL
 	protected String			link;
@@ -43,6 +53,6 @@ public class Lecture extends AbstractEntity {
 	@ManyToOne(optional = false)
 	@NotNull
 	@Valid
-	protected Course			course;
+	protected Lecturer			lecturer;
 
 }
