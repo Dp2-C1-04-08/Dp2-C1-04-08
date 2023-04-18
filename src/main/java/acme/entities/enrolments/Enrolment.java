38428ec855e44145b34b852e8c94b67be947a711
Ernesto <ertorivero2661@gmail.com>
2023-04-18 19:38:43 +0200

    Register an enrolment funcional

--- src/main/java/acme/entities/enrolments/Enrolment.java
+++ src/main/java/acme/entities/enrolments/Enrolment.java
@@ -4,7 +4,6 @@ package acme.entities.enrolments;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -44,7 +43,7 @@ public class Enrolment extends AbstractEntity {
 
 	@NotNull
 	@Valid
-	@OneToOne(optional = false)
+	@ManyToOne(optional = false)
 	protected Student			student;
 
 	@NotNull
