8e7800ccd4b93a0cca61b900b7083edb3a3daa89
siadan <siadan@alum.us.es>
2023-05-24 19:23:28 +0200

    Created files for peep testing

---
+++ src/test/java/acme/testing/any/peep/AnyPeepListTest.java
@@ -0,0 +1,20 @@
+
+package acme.testing.any.peep;
+
+import acme.testing.TestHarness;
+
+public class AnyPeepListTest extends TestHarness {
+
+	public void test100Positive() {
+
+	}
+
+	public void test200Negative() {
+
+	}
+
+	public void test300Hacking() {
+
+	}
+
+}
