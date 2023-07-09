3631fab5f26ec643eb9629f3d12e39cb385f7da0
manbarjim2 <80253313+manbarjim2@users.noreply.github.com>
2023-07-10 00:37:11 +0200

    Corregido el D04 del estudiante 1

--- src/test/java/acme/testing/lecturer/lecture/LecturerLecturePublishTest.java
+++ src/test/java/acme/testing/lecturer/lecture/LecturerLecturePublishTest.java
@@ -1,14 +1,11 @@
 
 package acme.testing.lecturer.lecture;
 
-import java.util.Collection;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import acme.entities.courses.Lecture;
 import acme.testing.TestHarness;
 
 public class LecturerLecturePublishTest extends TestHarness {
@@ -23,18 +20,11 @@ public class LecturerLecturePublishTest extends TestHarness {
 
 	@ParameterizedTest
 	@CsvFileSource(resources = "/lecturer/lecture/publish-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-	public void test100Positive(final int lectureRecord, final int courseRecord, final String title) {
+	public void test100Positive(final int lectureRecord, final String title) {
 
 		super.signIn("lecturer1", "lecturer1");
 
-		super.clickOnMenu("Lecturer", "List Courses");
-		super.checkListingExists();
-		super.sortListing(0, "asc");
-
-		super.clickOnListingRecord(courseRecord);
-		super.checkFormExists();
-
-		super.clickOnButton("List Lecture");
+		super.clickOnMenu("Lecturer", "List my Lectures");
 		super.checkListingExists();
 		super.sortListing(0, "asc");
 		super.checkColumnHasValue(lectureRecord, 0, title);
@@ -58,28 +48,23 @@ public class LecturerLecturePublishTest extends TestHarness {
 	@Test
 	public void test300Hacking() {
 
-		Collection<Lecture> lectures;
-		String params;
+		String param;
 
-		lectures = this.repository.findLectureByLecturerResume("1");
-		for (final Lecture lecture : lectures)
-			if (lecture.isDraft()) {
-				params = String.format("id=%d", lecture.getId());
+		param = String.format("masterId=%d", 108);
 
-				super.checkLinkExists("Sign in");
-				super.request("/lecturer/lecture/publish", params);
-				super.checkPanicExists();
+		super.checkLinkExists("Sign in");
+		super.request("/lecturer/lecture/publish", param);
+		super.checkPanicExists();
 
-				super.signIn("administrator", "administrator");
-				super.request("/lecturer/lecture/publish", params);
-				super.checkPanicExists();
-				super.signOut();
+		super.signIn("administrator", "administrator");
+		super.request("/lecturer/lecture/publish", param);
+		super.checkPanicExists();
+		super.signOut();
 
-				super.signIn("company1", "company1");
-				super.request("/lecturer/lecture/publish", params);
-				super.checkPanicExists();
-				super.signOut();
-			}
+		super.signIn("company1", "company1");
+		super.request("/lecturer/lecture/publish", param);
+		super.checkPanicExists();
+		super.signOut();
 	}
 
 }
